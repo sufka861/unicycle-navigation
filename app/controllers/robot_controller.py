b72ca39880aa2197604cad9a8dc30544dd29418e
sufka861 <sufkarmon2@gmail.com>
2023-09-04 18:53:38 +0300

    split up robot controller into smaller functions

--- app/controllers/robot_controller.py
+++ app/controllers/robot_controller.py
@@ -15,7 +15,7 @@ def create_robots(num_robots, data):
     return robot_list
 
 
-def calculate_movement(bot, robot_list, circ_x, circ_y, radius):
+def close_obst_count(bot, num_circ_obsts, circ_x, circ_y, radius):
     close_obst = []
     dist = []
     for i in range(num_circ_obsts):
@@ -23,13 +23,10 @@ def calculate_movement(bot, robot_list, circ_x, circ_y, radius):
         if distance <= (skirt_r + radius[i]):
             close_obst.append([circ_x[i], circ_y[i], radius[i]])
             dist.append(distance)
-    if len(close_obst) == 0:  # No obstacle in sensor skirt
-        [v, omega] = bot.go_to_goal()  # Output from controller go_to_goal()
-    else:
-        closest_obj = dist.index(min(dist))  # Index of the closest object
-        obstX = np.array([circ_x[closest_obj], circ_y[closest_obj]])
-        [v, omega] = bot.avoid_obst(obstX)
+    return (close_obst, dist)
 
+
+def min_distance_from_other_robot(bot, robot_list):
     # Avoid other robots
     min_distance = float('inf')  # Initialize to positive infinity
     for other_robot in robot_list:
@@ -38,6 +35,21 @@ def calculate_movement(bot, robot_list, circ_x, circ_y, radius):
             if distance < min_distance:
                 min_distance = distance
                 other_robotX = [other_robot.x, other_robot.y]
+    return (min_distance, other_robotX)
+
+
+def calculate_movement(bot, robot_list, circ_x, circ_y, radius):
+    # Go to gaol while avoiding obstacles
+    [close_obst, dist] = close_obst_count(bot, num_circ_obsts, circ_x, circ_y, radius)
+    if len(close_obst) == 0:  # No obstacle in sensor skirt
+        [v, omega] = bot.go_to_goal()  # Output from controller go_to_goal()
+    else:
+        closest_obj = dist.index(min(dist))  # Index of the closest object
+        obstX = np.array([circ_x[closest_obj], circ_y[closest_obj]])
+        [v, omega] = bot.avoid_obst(obstX)
+
+    # Avoid other robots
+    [min_distance, other_robotX] = min_distance_from_other_robot(bot,robot_list)
     if min_distance < skirt_r:  # If another robot within radius, use avoid_obst with the position of the other robot
         [v, omega] = bot.avoid_obst(other_robotX)
     return [v, omega]
